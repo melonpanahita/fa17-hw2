ea390c7aab974f63187e70a7b3dc65c81473150a
Anahita <afarshi@berkeley.edu>
2017-09-27 16:39:01 -0700

    done

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(baz)
+  	@baz = baz
+  end
+
+  def bar(hash, symbols)
+  	hash.to_s + symbols[:sat].to_s + symbols[:dat].to_s
   end
 end
--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -8,5 +8,10 @@ class Stringify
 
   def what_am_i
   	# YOUR IMPLEMENTATION HERE
+  	if @name.blank? || @adjective.blank?
+  		return "You are nothing!"
+  	else
+  		return "<em>your_name</em> is so <em>your_adjective</em>"
+  	end
   end
 end
